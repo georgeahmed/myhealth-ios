bd6d26c1377156a6a4a1380e73dcdda765de22df
Work <test@tests-MacBook-Air.local>
2025-05-20 18:00:03 +0100

    Initial iOS commit

---
+++ MyHealthiOS/MyHealthiOS/MyHealthiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthiOSApp.swift
+//  MyHealthiOS
+//
+//  Created by Work on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyHealthiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
